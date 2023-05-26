4d03568815f080a18ddd52fd80c5cf942b39da73
Lucas G. Cordeiro <lucasgon192@gmail.com>
2023-05-26 07:32:42 -0400

    fix: removed assertSame from UpperLower

--- src/main/java/vvt/trabalho/UpperLower.java
+++ src/main/java/vvt/trabalho/UpperLower.java
@@ -1,14 +1,6 @@
 package vvt.trabalho;
 
 public class UpperLower {
-    public static String stringToUpperByReference(String string) {
-        return string.toUpperCase();
-    }
-
-    public static String stringToLowerByReference(String string) {
-        return string.toLowerCase();
-    }
-    
     public static String stringCopyToUpper(String string) {
         String stringCopy = new String(string);
         return stringCopy.toLowerCase();
